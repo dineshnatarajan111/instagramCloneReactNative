b680038470e6bb886c5ac80cc09d7b1acfbd254e
dineshnatarajan111 <dineshnatarajan5@gmail.com>
2021-06-29 13:08:24 +0530

    added camera

--- App.js
+++ App.js
@@ -3,15 +3,21 @@ import {
   SafeAreaView,
   View,
   Text,
+  StatusBar,
   StyleSheet,
 } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import SwitchNavigator from './src/navigation/switchNavigation';
+import { Platform } from 'react-native';
 
 const App = () => {
   return(
     // <SafeAreaView style={styles.container}>
       <NavigationContainer>
+        <StatusBar 
+        barStyle = "dark-content"
+        backgroundColor = {Platform.OS == "android" ? "#FFF" : null}
+        />
         <SwitchNavigator />
       </NavigationContainer>
     // </SafeAreaView>
--- src/screens/camera/index.js
+++ src/screens/camera/index.js
@@ -1,14 +1,37 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {
     View,
     Text,
     StyleSheet,
     SafeAreaView,
 } from 'react-native'
+import { RNCamera } from 'react-native-camera';
 
 const Camera = () => {
+    const [cameraRef,setCameraRef] = useState(null);
     return(
-        <SafeAreaView style={styles.container}></SafeAreaView>
+        <View style={styles.container}>
+            <RNCamera
+            ref={ref => {
+                setCameraRef(ref);
+            }}
+            style={styles.preview}
+            type={RNCamera.Constants.Type.back}
+            flashMode={RNCamera.Constants.FlashMode.on}
+            androidCameraPermissionOptions={{
+                title: 'Permission to use camera',
+                message: 'We need your permission to use your camera',
+                buttonPositive: 'Ok',
+                buttonNegative: 'Cancel',
+            }}
+            androidRecordAudioPermissionOptions={{
+                title: 'Permission to use audio recording',
+                message: 'We need your permission to use your audio',
+                buttonPositive: 'Ok',
+                buttonNegative: 'Cancel',
+            }}
+            />
+        </View>
     );
 }
 
@@ -16,6 +39,9 @@ const styles = StyleSheet.create({
     container:{
         flex : 1,
     },
+    preview : {
+        flex : 1,
+    }
 });
 
 export default Camera;
