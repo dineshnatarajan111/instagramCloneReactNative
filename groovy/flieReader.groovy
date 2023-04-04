6e051213a0a3760e0657200c37b3a139929961fd
dineshnatarajan111 <dineshnatarajan5@gmail.com>
2023-04-04 15:34:43 +0530

    update

--- groovy/flieReader.groovy
+++ groovy/flieReader.groovy
@@ -1,74 +1,32 @@
-import groovy.util.XmlSlurper
-import java.util.Map
-import jenkins.*
-import jenkins.model.*
-import hudson.*
-import hudson.model.*
+import org.codehaus.groovy.scriptom.ActiveXObject
 
-import java.io.*
-import java.text.SimpleDateFormat
-import java.util.Calendar
-import org.apache.poi.ss.usermodel.*
-import org.apache.poi.hssf.usermodel.*
-import org.apache.poi.xssf.usermodel.*
-import org.apache.poi.ss.util.*
+def read_excel(){
+    def excelObj = new ActiveXObject('Excel.Application')
 
-import org.codehaus.groovy.scriptom.*
+    def workBook = excelObj.Workbooks.Open("GPF_KLI_Dummy.xlsx")
 
-// def read_excel(){
-//     env.WORKSPACE = pwd()
-//     def version = readFile "${env.WORKSPACE}/GPF_KLI_Dummy.xlsx"
+    //To get a sheet by index
+    def sheetByIndex = workBook.Sheets.Item[1]
 
-// }
+    print(sheetByIndex)
+    
+    //To get the active sheet in the workbook
+    def sheetActive = workBook.ActiveSheet
 
-def read_excel() {
+    print(sheetActive)
+    
+    //To get the sheet by name
+    def sheetByName = workBook.Sheets('Sheet1')
 
-    def fs = new FileInputStream("GPF_KLI_Dummy.xlsx")
+    print(sheetByName)
 
-    Workbook wb = WorkbookFactory.create(fs);
-    def ws = wb.getSheet("Sheet1");
-    int r = ws.getLastRowNum();
-    testRunner.testCase.getTestStepByName("Properties").setPropertyValue("DataRowCount", r.toString());
-    for (def i = 1; i <= r; i++) {
-        def row = ws.getRow(i)
-        def headerRow = ws.getRow(0)
-        def noOfCell = row.getLastCellNum();
-        for (def j = 0; j < noOfCell; j++) {
-            def headertext = ws.getRow(0).getCell(j).getStringCellValue()
-            Cell cell = row.getCell(j)
-            if (cell == null || cell.getCellType() == Cell.CELL_TYPE_BLANK) {
-                value = "";
-            } else if (cell.getCellType() == Cell.CELL_TYPE_BOOLEAN) {
-                value = (cell.getBooleanValue());
-            } else if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC) {
-                if (DateUtil.isCellDateFormatted(cell)) {
-                    Date date = cell.getDateCellValue();
-                    SimpleDateFormat format1 = new SimpleDateFormat("dd-MM-yyyy");
-                    String formatted = format1.format(date);
-                    value = formatted
-                } else {
-                    value = (cell.getNumericCellValue());
-                }
-            } else if (cell.getCellType() == Cell.CELL_TYPE_STRING) {
-                value = (cell.getStringCellValue());
-            } else if (cell.getCellType() == Cell.CELL_TYPE_ERROR) {} else if (cell.getCellType() == Cell.CELL_TYPE_FORMULA) {
-                value = String.valueOf(cell.getStringCellValue())
-            }
-            testRunner.testCase.getTestStepByName("Properties").setPropertyValue(headertext.toString() + "" + i.toString(), value.toString())
-        }
-    }
+    def cellValue = sheet.Cells(2,1).Value
 
-
-    //am based condition
-    for (int i = 1; i <= 4; i++) {
-        if (context.expand('${Properties#Firstname' + i.toString() + '}').toLowerCase().contains("am")) {
-            log.info context.expand('${Properties#Firstname' + i.toString() + '}') + " " + context.expand('${Properties#Lastname' + i.toString() + '}')
-        }
-    }
+    print(cellValue)
 }
 
 def sample(){
-    print("Hello!World")
+    print("Hello World")
 }
 
 return this
